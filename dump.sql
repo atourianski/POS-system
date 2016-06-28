d716e756655b898b659988de82c3da65e154c602
atourianski <a.tourianski@gmail.com>
2016-06-28 16:25:06 -0400

    update sql file wip

--- dump.sql
+++ dump.sql
@@ -16,30 +16,98 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `potluck`
+-- Table structure for table `food`
 --
 
-DROP TABLE IF EXISTS `potluck`;
+DROP TABLE IF EXISTS `food`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `potluck` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  `food` varchar(30) DEFAULT NULL,
-  `drink` varchar(30) DEFAULT NULL,
-  `confirmed` char(1) DEFAULT NULL,
-  `signup_data` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `food` (
+  `belyash` int(2) NOT NULL DEFAULT '5',
+  `cheb` int(2) DEFAULT NULL,
+  `borcht` int(2) DEFAULT NULL,
+  `salyan` int(2) DEFAULT NULL,
+  `fish_salyan` int(2) DEFAULT NULL,
+  `akrosh` int(2) DEFAULT NULL,
+  `harcho` int(2) DEFAULT NULL,
+  `pea` int(2) DEFAULT NULL,
+  `greek` int(2) DEFAULT NULL,
+  `beet` int(2) DEFAULT NULL,
+  `olivier` int(2) DEFAULT NULL,
+  `cake` int(2) DEFAULT NULL,
+  `pel` int(2) DEFAULT NULL,
+  `vareniki` int(2) DEFAULT NULL,
+  `salo` int(2) DEFAULT NULL,
+  `shuba` int(2) DEFAULT NULL,
+  `pastilla` int(2) DEFAULT NULL,
+  `fish` int(2) DEFAULT NULL,
+  `strog` int(2) DEFAULT NULL,
+  `kebab` int(2) DEFAULT NULL,
+  `veg_plate` int(2) DEFAULT NULL,
+  `plov` int(2) DEFAULT NULL,
+  `tabak` int(2) DEFAULT NULL,
+  `beef` int(2) DEFAULT NULL,
+  `kotlet` int(2) DEFAULT NULL,
+  `saus` int(2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `potluck`
+-- Dumping data for table `food`
 --
 
-LOCK TABLES `potluck` WRITE;
-/*!40000 ALTER TABLE `potluck` DISABLE KEYS */;
-/*!40000 ALTER TABLE `potluck` ENABLE KEYS */;
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `foodstuffs`
+--
+
+DROP TABLE IF EXISTS `foodstuffs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `foodstuffs` (
+  `name` varchar(20) DEFAULT NULL,
+  `price` int(2) DEFAULT NULL,
+  `notes` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `foodstuffs`
+--
+
+LOCK TABLES `foodstuffs` WRITE;
+/*!40000 ALTER TABLE `foodstuffs` DISABLE KEYS */;
+INSERT INTO `foodstuffs` VALUES ('belyash',5,'fried meat donut'),('cheb',5,'cheburek, georgian deep fried savoury pastry'),('borcht',6,'russian soup'),('salyan',6,'russian soup');
+/*!40000 ALTER TABLE `foodstuffs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoice_id`
+--
+
+DROP TABLE IF EXISTS `invoice_id`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoice_id` (
+  `banya` float(6,2) DEFAULT NULL,
+  `food` float(6,2) DEFAULT NULL,
+  `drink` float(6,2) DEFAULT NULL,
+  `massage` float(6,2) DEFAULT NULL,
+  `misc` float(6,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoice_id`
+--
+
+LOCK TABLES `invoice_id` WRITE;
+/*!40000 ALTER TABLE `invoice_id` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoice_id` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -78,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-28 18:22:24
+-- Dump completed on 2016-06-28 20:23:26
