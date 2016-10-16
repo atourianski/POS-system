9ef58fbea7a5125cb0796edd10528f9bd86e663e
zramsay <zach@monax.io>
2016-10-16 15:33:37 -0400

    almost there

--- go-server/main.go
+++ go-server/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/http"
+	//"reflect"
 	"strconv"
 	"strings"
-
-	"fmt"
-	"net/http"
 	"time"
+
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
@@ -46,7 +49,6 @@ func main() {
 	http.HandleFunc("/addItems", addItems)
 	http.HandleFunc("/closeSession", closeSession)
 
-	// serve it all up
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -55,8 +57,28 @@ var defaultHTML = `<!DOCTYPE html>
 <head>
 	<meta charset="UTF-8">
 	<title></title>
-</head>
-`
+</head>`
+
+type ActiveLockers struct {
+	Bracelet_num string
+	Entry_time   string
+}
+
+type FoodAvailable struct {
+	Name  string
+	Price string // convert to int...or marshal as such?
+}
+
+type DrinksAvailable struct {
+	Name  string
+	Price string // convert to int...or marshal as such?
+}
+
+var openSessionsHTML = `<h2>Open Sessions</h2>
+Entry Time | Locker Number<br>
+{{range $index, $element := .}}
+<b>{{ .Entry_time}}</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ .Bracelet_num}}<br>
+{{end}}`
 
 // TODO use some fucking templates
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -64,56 +86,126 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func newSession(w http.ResponseWriter, r *http.Request) {
-	htmlToWrite := defaultHTML + `<body>
+	var tpl = defaultHTML + `<body>
 	<h1>New Session</h1>
 	Enter an available locker number:
 	<form method="POST" action="/initializeSession">
 	<input type="number" name="bracelet_id">
-	<input type="submit" value="Submit">
+	<input type="submit" value="Submit"><br>
+	<br>
+	<a href="/">Home</a>` + openSessionsHTML + `
 </body>
 </html>`
-	w.Write([]byte(htmlToWrite))
-	//http.ServeFile(w, r, "new_session.html")
+
+	t := template.New("t")
+	t, err = t.Parse(tpl)
+	if err != nil {
+		panic(err)
+	}
+
+	Lockers := getActiveSession()
+
+	var doc bytes.Buffer
+	if err := t.Execute(&doc, Lockers); err != nil {
+		panic(err)
+	}
+	w.Write(doc.Bytes())
 }
 
 func addItems(w http.ResponseWriter, r *http.Request) {
-	htmlToWrite := defaultHTML + `<body>
+
+	foodTemplate := `<b>Food:</b><br>
+		{{range $index, $element := .}}
+		{{ .Name}} $ {{ .Price}}<br>
+		<input type="number" name="{{ .Name}}"><br>
+		{{end}}
+		<br>
+		`
+
+	tFood := template.New("tFood")
+	tFood, err = tFood.Parse(foodTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	Foods := getActiveFoodstuffs()
+
+	var doc bytes.Buffer
+	if err := tFood.Execute(&doc, Foods); err != nil {
+		panic(err)
+	}
+	foodHTML := doc.String()
+
+	// ------------ TODO deduplicate w/ above ------
+
+	var drinksTemplate = `<b>Drinks:</b><br>
+		{{range $index, $element := .}}
+		{{ .Name}} $ {{ .Price}}<br>
+		<input type="number" name="{{ .Name}}"><br>
+		{{end}}
+		<br>
+		`
+
+	tDrinks := template.New("tDrinks")
+	tDrinks, err = tDrinks.Parse(drinksTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	Drinks := getActiveDrinks()
+
+	var doc1 bytes.Buffer
+	if err := tDrinks.Execute(&doc1, Drinks); err != nil {
+		panic(err)
+	}
+	drinksHTML := doc1.String()
+
+	// the rendered display
+	var htmlToWrite = defaultHTML + `<body>
 	<h1>Add Some Items</h1>
 	<form method="POST" action="/addItemsToASession">
 		Locker Number:<br>
 		<input type="number" name="bracelet_id"><br>
+		<br>
+		` + foodHTML + `<br>
+		<br>
+		` + drinksHTML + `<br>
 
-		Soup:<br>
-		<input type="number" name="soup"><br>
-
-		Beer:<br>
-		<input type="number" name="beer"><br>
-
-		Scrub:<br>
-		<input type="number" name="scrub"><br>
+		Misc: <b>must set dollar amount</b><br>
+		<input type="number" name="misc"><br>
 
 	<input type="submit" value="Submit">
 	</form><br>
 	<a href="/">Home</a>
 </body>
 </html>`
-
 	w.Write([]byte(htmlToWrite))
-	//http.ServeFile(w, r, "add_items.html")
 }
 
 func closeSession(w http.ResponseWriter, r *http.Request) {
-	htmlToWrite := defaultHTML + `<body>
-	<h1>Close A Session</h1>
-	<form method="POST" action="/displayBill">
-	<input type="number" name="bracelet_id">
-	<input type="submit" value="Submit">
-	</form><br>
-	<a href="/">Home</a>
-</body>
-</html>`
-	w.Write([]byte(htmlToWrite))
-	//http.ServeFile(w, r, "close_session.html")
+	var tpl = defaultHTML + `<body>
+		<h1>Close A Session</h1>
+		<form method="POST" action="/displayBill">
+		<input type="number" name="bracelet_id">
+		<input type="submit" value="Submit">
+		</form><br>
+		<a href="/">Home</a><br>` + openSessionsHTML + `
+	</body>
+	</html>`
+
+	Lockers := getActiveSession()
+
+	t := template.New("t")
+	t, err = t.Parse(tpl)
+	if err != nil {
+		panic(err)
+	}
+
+	var doc bytes.Buffer
+	if err := t.Execute(&doc, Lockers); err != nil {
+		panic(err)
+	}
+	w.Write(doc.Bytes())
 }
 
 func initializeSession(w http.ResponseWriter, r *http.Request) {
@@ -163,25 +255,68 @@ func addItemsToASession(w http.ResponseWriter, r *http.Request) {
 			return // re-direct ... ?
 		}
 
-		foodPrice := 8
-		drinkPrice := 5
-		miscPrice := 3
+		orders := map[string]int{}
 
-		f, _ := strconv.Atoi(r.FormValue("soup"))
-		d, _ := strconv.Atoi(r.FormValue("beer"))
-		m, _ := strconv.Atoi(r.FormValue("scrub"))
+		// MISC
+		miscellaneous, _ := strconv.Atoi(r.FormValue("misc"))
+		orders["misc"] = miscellaneous
 
-		food := f * foodPrice
-		drink := d * drinkPrice
-		misc := m * miscPrice
+		// FOOD
+		Foods := getActiveFoodstuffs()
+		totalsFood := 0
+		// iterate through map, match "set" items from user input w/ "active" items from db
+		for _, activeFoods := range Foods {
+			numberOrdered := r.FormValue(activeFoods.Name)
+			if numberOrdered != "" && numberOrdered != "0" {
+				num0, _ := strconv.Atoi(numberOrdered)
+				price, _ := strconv.Atoi(activeFoods.Price)
+				totalsFood = totalsFood + (num0 * price)
+			}
+		}
+		orders["food"] = totalsFood
 
-		// XXX need to read from this row first & add to total, rather than overwrite!!!!
+		// DRINK
+		Drinks := getActiveDrinks()
+		totalsDrink := 0
+		// iterate through map, match "set" items from user input w/ "active" items from db
+		for _, activeDrinks := range Drinks {
+			numberOrdered := r.FormValue(activeDrinks.Name)
+			if numberOrdered != "" && numberOrdered != "0" {
+				num0, _ := strconv.Atoi(numberOrdered)
+				price, _ := strconv.Atoi(activeDrinks.Price)
+				totalsDrink = totalsDrink + (num0 * price)
+			}
+		}
+		orders["drink"] = totalsDrink
+
+		// get current invoice
+		var curFood string
+		var curDrink string
+		var curMisc string
+
+		// here we read in some totals & add to them with a new purchase
+		err := db.QueryRow("SELECT food, drink, misc FROM invoices WHERE bracelet_id=? AND invoice_id=?", braceletID, invoiceID).Scan(&curFood, &curDrink, &curMisc)
+		if err != nil {
+			panic(err)
+		}
+		// convert to int
+		curF, _ := strconv.Atoi(curFood)
+		curD, _ := strconv.Atoi(curDrink)
+		curM, _ := strconv.Atoi(curMisc)
+
+		// add to what was just ordered
+		food := orders["food"] + curF
+		drink := orders["drink"] + curD
+		misc := orders["misc"] + curM
+
+		// update invoice with food, drink, misc
 		_, err = db.Exec("UPDATE invoices SET food=?,drink=?,misc=? WHERE invoice_id=?", food, drink, misc, invoiceID)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("error updating invoice: %v", err), http.StatusInternalServerError)
 			return
 		}
 
+		time.Sleep(time.Second * 1)
 		http.Redirect(w, r, "/addItems", 301)
 	}
 }
@@ -203,7 +338,7 @@ func displayBill(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
-
+		// XXX this is hacky & can probably be cleaned up
 		// query invoice ID to get bill info & display
 		var banyaCost string
 		var foodCost string
@@ -214,6 +349,13 @@ func displayBill(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 
+		bCost, _ := strconv.Atoi(banyaCost)
+		fCost, _ := strconv.Atoi(foodCost)
+		dCost, _ := strconv.Atoi(drinkCost)
+		mCost, _ := strconv.Atoi(miscCost)
+
+		totalCost := bCost + fCost + dCost + mCost
+
 		someHTML := `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -221,15 +363,23 @@ func displayBill(w http.ResponseWriter, r *http.Request) {
 	<title></title>
 </head>
 <body>
-	<h1>Bracelet #: ` + braceletID + `</h1>
-	<h1>Invoice #: ` + invoiceID + `</h1>
-	<h2>Entry Time: ` + entryTime + `</h2>
-	<h1>Bill:</h1>
-	<h2>banya: ` + banyaCost + `</h2>
-	<h2>food: ` + foodCost + `</h2>
-	<h2>drink: ` + drinkCost + `</h2>
-	<h2>misc: ` + miscCost + `</h2>
+	Bracelet #: ` + braceletID + `<br>
+	Invoice #: ` + invoiceID + `<br>
+	Entry Time: ` + entryTime + `<br>
 
+	<br>
+	<b>Bill:</b><br>
+	<br>
+
+	Banya: ` + banyaCost + `<br>
+	Food: ` + foodCost + `<br>
+	Drink: ` + drinkCost + `<br>
+	Misc: ` + miscCost + `<br>
+
+	<br>
+	<b>Total: $ ` + strconv.Itoa(totalCost) + `</b><br>
+	<br>
+	
 	<form method="POST" action="/closeBill?` + braceletID + `&` + invoiceID + `">
 	<input type="submit" value="Close Bill">
 	</form><br>
@@ -243,6 +393,7 @@ func displayBill(w http.ResponseWriter, r *http.Request) {
 
 func closeBill(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+		// hacky
 		theURL := r.URL.String()
 		getFields := strings.Split(theURL, "?")[1]
 		braceletID := strings.Split(getFields, "&")[0]
@@ -254,6 +405,8 @@ func closeBill(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, fmt.Sprintf("error closing invoice: %v", err), http.StatusInternalServerError)
 			return
 		}
+
+		time.Sleep(time.Second * 1)
 		http.Redirect(w, r, "/", 301)
 	}
 }
@@ -273,6 +426,66 @@ func doesSessionAlreadyExist(braceletID string) (bool, string) {
 		// bracelet ID does exist - do not create new session
 		return true, invoiceID
 	}
+}
+
+func getActiveSession() []*ActiveLockers {
+	rows, err := db.Query("SELECT bracelet_num, entry_time FROM visit WHERE active=1")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var Lockers []*ActiveLockers
+
+	for rows.Next() {
+		l := new(ActiveLockers)
+		err = rows.Scan(&l.Bracelet_num, &l.Entry_time)
+		if err != nil {
+			panic(err)
+		}
+		Lockers = append(Lockers, l)
+	}
+	return Lockers
+}
+
+func getActiveFoodstuffs() []*FoodAvailable {
+	rows, err := db.Query("SELECT name, price FROM foodstuffs WHERE active=1")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var Foods []*FoodAvailable
+
+	for rows.Next() {
+		f := new(FoodAvailable)
+		err = rows.Scan(&f.Name, &f.Price)
+		if err != nil {
+			panic(err)
+		}
+		Foods = append(Foods, f)
+	}
+	return Foods
+}
+
+func getActiveDrinks() []*DrinksAvailable {
+	rows, err := db.Query("SELECT name, price FROM drinks WHERE active=1")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var Drinks []*DrinksAvailable
+
+	for rows.Next() {
+		d := new(DrinksAvailable)
+		err = rows.Scan(&d.Name, &d.Price)
+		if err != nil {
+			panic(err)
+		}
+		Drinks = append(Drinks, d)
+	}
+	return Drinks
 }
 
 func getFormattedDate() string {
